a3d7fae0fbeb2d63f568aa27f270380fe93d4f33
eddieliao <54926923+eddieliao@users.noreply.github.com>
2022-09-22 16:27:55 -0500

    Added support for Artemis65 keyboard

---
+++ keyboards/artemis65/config.h
@@ -0,0 +1,32 @@
+// Copyright 2022 Eddie Liao (@eddieliao)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// /* Matrix Size */
+// #define MATRIX_ROWS 5
+// #define MATRIX_COLS 14
+
+// /* Matrix Assignments */
+// #define MATRIX_ROW_PINS { C7, C6, B6, B5, B4 }
+// #define MATRIX_COL_PINS { D1, D0, D2, D3, D5, B7, B0, E6, F0, F1, F4, F5, F6, F7 }
+// #define UNUSED_PINS { D6, D7 }
+
+// /* Diode Direction */
+// #define DIODE_DIRECTION COL2ROW
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
